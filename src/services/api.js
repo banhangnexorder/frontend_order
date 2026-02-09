2a7f2cd0503f64ce24cc02f49e9cc57ca53423ca
tha_nguyen <>
2026-02-09 11:48:40 +0700

    fix api base url with env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://backend-order-dev.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL, // ✅ KHÔNG HARDCODE
 });
 
 // 🔐 GẮN TOKEN TỰ ĐỘNG
