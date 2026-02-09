10d5e9fd80adc8510c61d026bc5ad50b4bc6efde
tha_nguyen <>
2026-02-09 13:56:59 +0700

    fix http => https

--- src/utils/menuImage.js
+++ src/utils/menuImage.js
@@ -1,4 +1,4 @@
-const BASE = "http://backend-order-dev.up.railway.app/uploads/menu";
+const BASE = "https://backend-order-dev.up.railway.app/uploads/menu";
 const DEFAULT = `https://shorthand.com/the-craft/raster-images/assets/5kVrMqC0wp/sh-unsplash_5qt09yibrok-4096x2731.jpeg`;
 
 export function getMenuImage(image) {
