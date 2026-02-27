f85e8577a19d028267d48df6f7eb52ba847cfaef
tha_nguyen <>
2026-02-27 10:16:38 +0700

    fix get image

--- src/utils/menuImage.js
+++ src/utils/menuImage.js
@@ -4,5 +4,5 @@ export function getMenuImage(image) {
   if (!image) return DEFAULT;
   if (image.startsWith("http")) return image;
 
-  return `${BASE}/uploads/menu/${image}`;
+  return `${BASE}/uploads/menu/${image}.webp`;
 }
