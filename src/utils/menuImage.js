459450f59b266a6a5c0248017bebc33c8f90c4ec
tha_nguyen <>
2026-02-27 10:06:02 +0700

    fix get image

--- src/utils/menuImage.js
+++ src/utils/menuImage.js
@@ -1,7 +1,8 @@
-const BASE = `${import.meta.env.VITE_API_URL}/uploads/menu`;
-const DEFAULT = `https://shorthand.com/the-craft/raster-images/assets/5kVrMqC0wp/sh-unsplash_5qt09yibrok-4096x2731.jpeg`;
+const BASE = import.meta.env.VITE_API_URL;
 
 export function getMenuImage(image) {
   if (!image) return DEFAULT;
-  return `${BASE}/${image}`;
+  if (image.startsWith("http")) return image;
+
+  return `${BASE}/uploads/menu/${image}`;
 }
