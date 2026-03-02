8a496e36efd56bcb70c6f0cb163dfa60a39d293e
tha_nguyen <>
2026-03-02 08:58:30 +0700

    fix format price

--- src/utils/todayStr.jsx
+++ src/utils/todayStr.jsx
@@ -1,5 +1,6 @@
 export const todayStr = () => {
   const now = new Date();
   now.setHours(12, 0, 0, 0); // chống lệch timezone
+  console.log("🔥 todayStr:",now.toISOString().slice(0, 10));
   return now.toISOString().slice(0, 10);
 };
